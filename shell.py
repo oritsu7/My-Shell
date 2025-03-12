5d9ead5bc72603d817a5b12cf43c0289e010f5fe
oritsu7 <oritsuminamoto@gmail.com>
2025-03-12 21:50:41 +0530

    added exit command and now checks for valid command

--- shell.py
+++ shell.py
@@ -1,7 +1,16 @@
+is_command = False
 
 while True:
     # taking the user input
     usinp = input("$ ")
     # check if echo is at the begining of the user input
     if usinp[0:5] == "echo ":
+        is_command = True
         print(usinp[5:])
+
+    if usinp[0:4] == "exit":
+        is_command = True
+        break
+
+    if not is_command:
+        print(usinp + " : command not found")
