cd067c50e339fc9703ad6eea13b8b9b46bf3264a
oritsu7 <oritsuminamoto@gmail.com>
2025-03-12 21:58:57 +0530

    date command

--- shell.py
+++ shell.py
@@ -1,16 +1,28 @@
+import datetime
+
+# bool var to check if the command is valid
 is_command = False
 
 while True:
+    # gets the current date and time 
+    dt = datetime.datetime.now()
+
     # taking the user input
     usinp = input("$ ")
+
     # check if echo is at the begining of the user input
     if usinp[0:5] == "echo ":
         is_command = True
         print(usinp[5:])
 
-    if usinp[0:4] == "exit":
+    # check if exit in user input
+    if usinp == "exit":
         is_command = True
         break
+
+    if usinp == "date":
+        is_command = True
+        print(f"{dt.day}-{dt.month}-{dt.year}")
 
     if not is_command:
         print(usinp + " : command not found")
